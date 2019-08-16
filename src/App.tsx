f7166407f0b0ee59049be0068f0cf5ae31fe14b4
lenconda <1053464288@qq.com>
2019-08-16 18:40:34 +0800

    :lipstick: Update styles add two sclasses

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.scss';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = (): JSX.Element => {
   return (
-    <div className="it-works">It works!</div>
+    <>
+    </>
   );
 };
 
