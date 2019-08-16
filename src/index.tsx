82b470dae2d550b9673b5ff1ef7a82bd4d7a77e8
lenconda <1053464288@qq.com>
2019-08-16 15:50:09 +0800

    :lipstick: Update css for test

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.scss';
+import 'lib-flexible';
 
 ReactDOM.render(<App />, document.getElementById('root'));
