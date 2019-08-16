c2f12d40264adbf731d871aec9a278f6cb57a53d
lenconda <1053464288@qq.com>
2019-08-17 00:23:13 +0800

    :lipstick: Update loading ui styles

--- src/effects/Loading/index.tsx
+++ src/effects/Loading/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 
 const Loading = () => (
-  <div className="gm-loading">
+  <div className="gm-loading bg-blue-orange">
     <img className="gm-loading-icon" src="/assets/img/loading_icon.svg" alt="加载中..." />
   </div>
 );
