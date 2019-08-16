842d994b66b34a6fc1b35659c688533335a0ac90
lenconda <1053464288@qq.com>
2019-08-16 20:53:18 +0800

    :hankey: Add a suspense fallback component

---
+++ src/effects/Loading/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+const Loading = () => (
+  <div className="gm-loading">
+    <img className="gm-loading-icon" src="/assets/img/loading_icon.svg" alt="加载中..." />
+  </div>
+);
+
+export default Loading;
