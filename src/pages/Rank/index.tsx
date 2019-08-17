214bc8945eb61169f2295f20ab334fc18aff802d
lenconda <1053464288@qq.com>
2019-08-17 12:12:58 +0800

    :lipstick: Fix UI layout for start and rank page

--- src/pages/Rank/index.tsx
+++ src/pages/Rank/index.tsx
@@ -11,18 +11,22 @@ const Rank = (props: RankPageProps): JSX.Element => {
       <img className="logo-text" src="/assets/img/logo_text.png" alt="地图猜猜猜" />
       <div className="content-card">
         <img className="logo-house" src="/assets/img/logo_rank.png" alt="House" />
-        <div className="brief-container grade">
-          <p className="brief-title">你的得分是：</p>
-          <p className="brief-content">
-            <span className="brief-grade-number">100</span>分
-          </p>
-        </div>
-        <div className="brief-container rank">
-          <p className="brief-title">超过全校</p>
-          <p className="brief-content">
-            <span className="brief-grade-number">78%</span>的人
-          </p>
-        </div>
+
+        <section className="rank-result">
+          <div className="brief-container grade">
+            <p className="brief-title">你的得分是：</p>
+            <p className="brief-content">
+              <span className="brief-grade-number">100</span>分
+            </p>
+          </div>
+          <div className="brief-container rank">
+            <p className="brief-title">超过全校</p>
+            <p className="brief-content">
+              <span className="brief-grade-number">78%</span>的人
+            </p>
+          </div>
+        </section>
+
         <Button className="start-game-button" largeFont>再玩一次</Button>
       </div>
     </div>
